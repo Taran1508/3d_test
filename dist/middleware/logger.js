affd6c2c540313a4469607440244340b21820920
John <john@Johns-MacBook-Air.local>
2025-08-16 19:22:02 +0530

    feat: Added frontend

--- dist/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = (req, res, next) => {
-    const now = new Date().toISOString();
-    console.log(`[${now}] ${req.method} ${req.originalUrl}`);
-    next();
-};
-exports.default = logger;
